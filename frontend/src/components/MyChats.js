9e8dceb87caf5d425d55b5c4e6afb351b85e88c0
KeshavSardana <keshav3619.be20@chitkara.edu.in>
2023-09-09 18:26:18 +0530

    cors error fix6

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -90,35 +90,36 @@ const MyChats = ({ fetchAgain }) => {
         overflowY="hidden"
       >
         {chats ? (
-          <Stack overflowY="scroll">
-            {chats.map((chat) => (
-              <Box
-                onClick={() => setSelectedChat(chat)}
-                cursor="pointer"
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                color={selectedChat === chat ? "white" : "black"}
-                px={3}
-                py={2}
-                borderRadius="lg"
-                key={chat._id}
-              >
-                <Text>
-                  {!chat.isGroupChat
-                    ? getSender(loggedUser, chat.users)
-                    : chat.chatName}
-                </Text>
-                {chat.latestMessage && (
-                  <Text fontSize="xs">
-                    <b>{chat.latestMessage.sender.name} : </b>
-                    {chat.latestMessage.content.length > 50
-                      ? chat.latestMessage.content.substring(0, 51) + "..."
-                      : chat.latestMessage.content}
-                  </Text>
-                )}
-              </Box>
-            ))}
-          </Stack>
+          <div>{chats}</div>
         ) : (
+          // <Stack overflowY="scroll">
+          //   {chats.map((chat) => (
+          //     <Box
+          //       onClick={() => setSelectedChat(chat)}
+          //       cursor="pointer"
+          //       bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+          //       color={selectedChat === chat ? "white" : "black"}
+          //       px={3}
+          //       py={2}
+          //       borderRadius="lg"
+          //       key={chat._id}
+          //     >
+          //       <Text>
+          //         {!chat.isGroupChat
+          //           ? getSender(loggedUser, chat.users)
+          //           : chat.chatName}
+          //       </Text>
+          //       {chat.latestMessage && (
+          //         <Text fontSize="xs">
+          //           <b>{chat.latestMessage.sender.name} : </b>
+          //           {chat.latestMessage.content.length > 50
+          //             ? chat.latestMessage.content.substring(0, 51) + "..."
+          //             : chat.latestMessage.content}
+          //         </Text>
+          //       )}
+          //     </Box>
+          //   ))}
+          // </Stack>
           <ChatLoading />
         )}
       </Box>
